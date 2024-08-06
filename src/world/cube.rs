9151c012c3283adea7d6cbbfe853fad3f4bab826
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-06 19:25:55 +0200

    Added constructor for cube

--- src/world/cube.rs
+++ src/world/cube.rs
@@ -1,3 +1,23 @@
+#[derive(PartialEq, Debug)]
 struct Cube {
     index: u8,
+}
+
+impl Cube {
+    pub fn new(index: u8) -> Self {
+        Cube { index }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_cube() {
+        let index = 2;
+        let result = Cube::new(index);
+        let expected = Cube { index };
+        assert_eq!(result, expected);
+    }
 }
