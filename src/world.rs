3fe0f9cfac62848505aafda34775ba90af37ae44
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-07 22:20:14 +0200

    Added tests for `create_chunk` and `set_chunk` methods, and fixed up their flaws accordingly

--- src/world.rs
+++ src/world.rs
@@ -18,13 +18,39 @@ impl World {
         }
     }
 
-    fn create_chunk_at(&mut self, position: I64Vec3) {
+    fn create_chunk(&mut self, chunk_position: &I64Vec3) {
         // Generate chunk data here!
         let new_chunk = Chunk::new();
-        self.set_chunk(position, new_chunk);
+        self.set_chunk(chunk_position, new_chunk);
     }
 
-    fn set_chunk(&mut self, position: I64Vec3, chunk: Chunk) {
-        let _ = self.data.insert(position, chunk);
+    fn set_chunk(&mut self, chunk_position: &I64Vec3, chunk: Chunk) {
+        let _ = self.data.insert(*chunk_position, chunk);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck::quickcheck;
+
+    use super::*;
+
+    quickcheck! {
+        fn create_chunk_at(random_x: i64, random_y: i64, random_z: i64) -> bool {
+            let mut world = World::new();
+            let position = I64Vec3::new(random_x, random_y, random_z);
+            world.create_chunk(&position);
+            world.data.get(&position).is_some()
+        }
+    }
+
+    quickcheck! {
+        fn set_chunk(random_x: i64, random_y: i64, random_z: i64) -> bool {
+            let mut world = World::new();
+            let position = I64Vec3::new(random_x, random_y, random_z);
+            let new_chunk = Chunk::new();
+            world.set_chunk(&position, new_chunk);
+            world.data.get(&position).is_some()
+        }
     }
 }
