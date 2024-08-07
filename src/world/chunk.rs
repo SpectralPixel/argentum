a0ba26a94e33662e2715717978338a8612b3682e
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-07 22:09:25 +0200

    Added helper functions to wrap positions into local and chunk coordinate systems

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -1,3 +1,4 @@
+use bevy::math::I64Vec3;
 use ndarray::{Array3, Ix3};
 
 use super::{cube::Cube, World};
@@ -13,10 +14,44 @@ impl Chunk {
         let empty_array: Array3<Cube> = Array3::from_elem(Ix3(size, size, size), Cube::default());
         Chunk { data: empty_array }
     }
+
+    pub fn world_to_chunk_position(world_position: I64Vec3) -> I64Vec3 {
+        let chunk_size = super::World::CHUNK_SIZE as i64;
+
+        I64Vec3::new(
+            Self::axis_to_chunk_position(world_position.x, chunk_size),
+            Self::axis_to_chunk_position(world_position.y, chunk_size),
+            Self::axis_to_chunk_position(world_position.z, chunk_size),
+        )
+    }
+
+    pub fn world_position_within_chunk(world_position: I64Vec3) -> I64Vec3 {
+        let chunk_size = super::World::CHUNK_SIZE as i64;
+
+        I64Vec3::new(
+            Self::wrap_position_into_chunk(world_position.x, chunk_size),
+            Self::wrap_position_into_chunk(world_position.y, chunk_size),
+            Self::wrap_position_into_chunk(world_position.z, chunk_size),
+        )
+    }
+
+    fn axis_to_chunk_position(axis_position: i64, chunk_size: i64) -> i64 {
+        axis_position / chunk_size
+    }
+
+    fn wrap_position_into_chunk(mut axis_position: i64, chunk_size: i64) -> i64 {
+        while axis_position < 0 {
+            axis_position += chunk_size;
+        }
+
+        axis_position % chunk_size
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use quickcheck::quickcheck;
+
     use super::*;
 
     #[test]
@@ -26,6 +61,57 @@ mod tests {
         let expected = Chunk {
             data: Array3::from_elem(Ix3(size, size, size), Cube::default()),
         };
+        assert_eq!(result, expected);
+    }
+
+    quickcheck! {
+        fn wrapped_position_within_chunk_bounds(random_x: i64, random_y: i64, random_z: i64) -> bool {
+            let position = I64Vec3::new(random_x, random_y, random_z);
+            let wrapped_position = Chunk::world_position_within_chunk(position);
+
+            let chunk_size = super::super::World::CHUNK_SIZE as i64;
+
+            let x_is_wrapped = wrapped_position.x >= 0 && wrapped_position.x < chunk_size;
+            let y_is_wrapped = wrapped_position.y >= 0 && wrapped_position.y < chunk_size;
+            let z_is_wrapped = wrapped_position.z >= 0 && wrapped_position.z < chunk_size;
+
+            x_is_wrapped && y_is_wrapped && z_is_wrapped
+        }
+    }
+
+    #[test]
+    fn axis_to_chunk_position_1() {
+        let position = 34;
+        let chunk_size = 32;
+        let result = Chunk::axis_to_chunk_position(position, chunk_size);
+        let expected = 1;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn axis_to_chunk_position_2() {
+        let position = 129;
+        let chunk_size = 32;
+        let result = Chunk::axis_to_chunk_position(position, chunk_size);
+        let expected = 4;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn wrap_position_into_chunk_1() {
+        let position = 34;
+        let chunk_size = 32;
+        let result = Chunk::wrap_position_into_chunk(position, chunk_size);
+        let expected = 2;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn wrap_position_into_chunk_2() {
+        let position = 41;
+        let chunk_size = 32;
+        let result = Chunk::wrap_position_into_chunk(position, chunk_size);
+        let expected = 9;
         assert_eq!(result, expected);
     }
 }
