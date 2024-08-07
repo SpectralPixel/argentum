06ebd744fc19bd4e4131353e49643c80c72cdf36
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-07 15:18:57 +0200

    Added default and new methods (identical for now)

--- src/world.rs
+++ src/world.rs
@@ -4,6 +4,13 @@ use chunk::Chunk;
 mod chunk;
 mod cube;
 
+#[derive(Default)]
 struct World {
     data: HashMap<Vec3, Chunk>,
+}
+
+impl World {
+    pub fn new() -> Self {
+        World { data: HashMap::default() }
+    }
 }
