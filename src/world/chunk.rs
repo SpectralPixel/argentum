ed5ac215f0b0acc796636c632a607c6f1ba63a63
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-07 09:55:33 +0200

    Created Chunk data structure with constructor Also implemented Default and Clone on Cube

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -1,1 +1,35 @@
+use ndarray::{Array3, Ix3};
 
+use super::cube::Cube;
+
+#[derive(PartialEq, Debug)]
+struct Chunk {
+    size: usize,
+    data: Array3<Cube>,
+}
+
+impl Chunk {
+    pub fn new(size: usize) -> Self {
+        let empty_array: Array3<Cube> = Array3::from_elem(Ix3(size, size, size), Cube::default());
+        Chunk {
+            size,
+            data: empty_array,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_chunk() {
+        let size = 2;
+        let result = Chunk::new(size);
+        let expected = Chunk {
+            size,
+            data: Array3::from_elem(Ix3(size, size, size), Cube::default()),
+        };
+        assert_eq!(result, expected);
+    }
+}
