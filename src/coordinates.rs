5535e82a1a5469a3981c5a80f5cdba1833d58030
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-09 18:15:54 +0200

    Re-exported CoordTypes under new names

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -2,6 +2,6 @@ mod chunk;
 mod global;
 mod local;
 
-pub use chunk::ChunkCoord;
-pub use global::GlobalCoord;
-pub use local::LocalCoord;
+pub use chunk::{ChunkCoord, CoordType as ChunkCoordType};
+pub use global::{GlobalCoord, CoordType as GlobalCoordType};
+pub use local::{LocalCoord, CoordType as LocalCoordType};
