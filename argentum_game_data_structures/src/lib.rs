c83bdf874356ae58dbbe09080d622a436d3d5474
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-25 16:44:51 +0200

    Explain module structure in comment

--- argentum_game_data_structures/src/lib.rs
+++ argentum_game_data_structures/src/lib.rs
@@ -9,4 +9,7 @@
 /// Import me!
 pub mod prelude;
 
+// Each following module corresponds to one crate.
+// Do not publish these modules, as that would cause confusion for the end
+// user. Please just place all your data structures in the prelude.
 mod ndarray;
