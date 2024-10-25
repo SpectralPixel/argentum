9792658ff470b57e0915f6262f82f0266316524b
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-25 16:42:33 +0200

    Document the crate root

--- argentum_game_data_structures/src/lib.rs
+++ argentum_game_data_structures/src/lib.rs
@@ -1,4 +1,12 @@
-//! This crate provides wrappers for data structures provided by dependencies. Each module corresponds to one dependency.
+//! # Argentum - Data structures
+//!
+//! This crate provides wrappers for data structures provided by other crates.
+//!
+//! Import the `prelude` to gain access to all the crate's types.
+//!
+//! For more information about Argentum, see the `argentum_game` crate.
 
-mod prelude;
+/// Import me!
+pub mod prelude;
+
 mod ndarray;
