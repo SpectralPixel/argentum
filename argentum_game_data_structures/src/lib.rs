7759e6184d0232e6efe55cb24f1558d25cb74aa0
Emil Engelbert <emil.engelbert@gmail.com>
2024-10-25 16:32:44 +0200

    Create prelude

--- argentum_game_data_structures/src/lib.rs
+++ argentum_game_data_structures/src/lib.rs
@@ -1,4 +1,6 @@
 //! This crate provides wrappers for data structures provided by dependencies. Each module corresponds to one dependency.
+
+mod prelude;
 mod ndarray;
 
 pub use ndarray::VoxelGrid;
